30a1582644257e805f2e8c3194cc5a6f0a771045
pramilaniroshan <pramilaniroshan@gmail.com>
2021-07-13 10:27:25 +0530

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,18 @@
 module.exports = {
   siteMetadata: {
     siteUrl: "https://www.yourdomain.tld",
-    title: "Gtasby Blog",
+    title: "Gatsby Blog",
   },
   plugins: [
     "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
-   "gatsby-plugin-sharp"
+   "gatsby-plugin-sharp",
+   {
+    resolve: "gatsby-source-filesystem",
+    options: {
+      name: `blog`,
+      path: `${__dirname}/blog`,
+    }
+  },
   ],
 };
