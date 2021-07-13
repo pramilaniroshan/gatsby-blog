5eac4c6b804961b7f558dd0499e41fe6bb19a561
pramilaniroshan <pramilaniroshan@gmail.com>
2021-07-13 10:09:18 +0530

    Finished Gatsby Tutorial Part 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,9 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "Gtasby Blog",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+   "gatsby-plugin-sharp"
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
-import * as React from "react"
-import { Link } from 'gatsby'
+import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
-
-// markup
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://cdn.pixabay.com/photo/2021/06/21/20/15/bee-6354562_960_720.jpg"
+      />
     </Layout>
   )
 }
